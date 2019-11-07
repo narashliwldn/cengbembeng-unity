2931d025a8fa1e82b04ccfbe0b6d5f846d02259c
narashliwldn <nahdiasholihah@gmail.com>
2019-11-07 11:06:13 +0700

    pertemuan ke? (animation2)

--- Assets/Scripts/PlayerAnimController.cs
+++ Assets/Scripts/PlayerAnimController.cs
@@ -14,8 +14,27 @@ public class PlayerAnimController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // player menembak
+        if(Input.GetKeyDown(KeyCode.Space)){
+            mbakAnimator.SetTrigger("isShooting");
+        }
+        // mematikan player dengan menekan BackSpace (tes doang)
         if(Input.GetKeyDown(KeyCode.Backspace)){
             mbakAnimator.SetTrigger("isDead");
+        }
+        // menjalankan player ke kanan
+        if(Input.GetKey(KeyCode.RightArrow)){
+            mbakAnimator.SetBool("isRunning", true);
+            transform.Translate(0.1f,0,0);
+        }
+        // menjalankan player ke kiri
+        if(Input.GetKey(KeyCode.LeftArrow)){
+            mbakAnimator.SetBool("isRunning", true);
+            transform.Translate(-0.1f,0,0);
+        }
+        // tombol ke kanan/kiri dilepas jadi false
+        if(Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.LeftArrow)){
+            mbakAnimator.SetBool("isRunning", false);
         }
     }
 }
